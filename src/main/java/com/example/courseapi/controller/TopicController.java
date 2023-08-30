da175b197077c28207319f196d82da8ae71a60af
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-08-30 17:02:27 +0300

    getAllTopics done

--- src/main/java/com/example/courseapi/controller/TopicController.java
+++ src/main/java/com/example/courseapi/controller/TopicController.java
@@ -3,11 +3,21 @@ package com.example.courseapi.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
     @RequestMapping("/topics")
-    public String getAllTopics() {
-        return "All Topics";
+//    getAllTopics to retun list of topics objects done
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("Spring", "Spring Framework", "SpringFramework Description"),
+                new Topic("Java", "Core Java", "Core Java Description"),
+                new Topic("javascript", "JavaScript", "JavaScript Description")
+
+
+        );
     }
 }
 
