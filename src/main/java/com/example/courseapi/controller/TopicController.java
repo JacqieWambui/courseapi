2dd6f15933b34a380eb297612d631c5bf1393b89
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-08-30 22:55:49 +0300

    updateTopic done

--- src/main/java/com/example/courseapi/controller/TopicController.java
+++ src/main/java/com/example/courseapi/controller/TopicController.java
@@ -2,10 +2,7 @@ package com.example.courseapi.controller;
 
 import com.example.courseapi.services.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +22,17 @@ public class TopicController {
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable  String id){
         return  topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public  void addTopic( @RequestBody  Topic topic) {
+        topicService.addTopic(topic);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id,topic);
     }
 }
 
--- src/main/java/com/example/courseapi/services/TopicService.java
+++ src/main/java/com/example/courseapi/services/TopicService.java
@@ -3,16 +3,17 @@ package com.example.courseapi.services;
 import com.example.courseapi.controller.Topic;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private  List<Topic> topics =  Arrays.asList(
+    private  List<Topic> topics = new ArrayList<>( Arrays.asList(
                 new Topic("Spring", "Spring Framework", "SpringFramework Description"),
                 new Topic("Java", "Core Java", "Core Java Description"),
                 new Topic("javascript", "JavaScript", "JavaScript Description")
-        );
+        ));
 
     //create a method that returns a list of topics
 
@@ -27,5 +28,24 @@ public class TopicService {
         get the id of the topics compare it with the id of topic passed, find the first onw then get
          */
         return topics.stream().filter( t-> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    /*
+    Loop through every topic depending on the size of the topic
+    for each topic in the list compare with the id, if its matches
+    update it
+     */
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)) {
+                topics.set(i,topic);
+                return;
+            }
+        }
     }
 }
