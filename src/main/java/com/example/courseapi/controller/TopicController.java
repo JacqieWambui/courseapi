e419bad4a5bf546dcb4f79ce8ccae27f5493ecb3
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-08-30 19:02:53 +0300

    getTopic done

--- src/main/java/com/example/courseapi/controller/TopicController.java
+++ src/main/java/com/example/courseapi/controller/TopicController.java
@@ -2,6 +2,8 @@ package com.example.courseapi.controller;
 
 import com.example.courseapi.services.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,20 +11,20 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/topics")
 public class TopicController {
 
     @Autowired  //declares this as a dependency
     private TopicService topicService;
-
+    @RequestMapping("/topics")
 //    getAllTopics to retun list of topics objects done
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
 
     //returns one topic
-    public Topic getTopic(String id){
-
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable  String id){
+        return  topicService.getTopic(id);
     }
 }
 
