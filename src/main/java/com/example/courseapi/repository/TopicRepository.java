a13203f2c5840a40297c216b5e16c8b0ba196011
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-08-31 09:17:12 +0300

    extends CrudRepository

--- src/main/java/com/example/courseapi/repository/TopicRepository.java
+++ src/main/java/com/example/courseapi/repository/TopicRepository.java
@@ -1,2 +1,13 @@
-package com.example.courseapi.repository;public class TopicRepository {
+package com.example.courseapi.repository;
+
+import com.example.courseapi.controller.Topic;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+    /*
+    getAllTopics()
+    getTopic(String id)
+    updateTopic(Topic t)
+    deleteTopic(String id)
+     */
 }
